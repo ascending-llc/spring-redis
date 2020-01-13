e02cf16081c7cfe79ca9bcc2df7aade307c8375b
ryo <ryo.hang@gmail.com>
2020-01-13 17:15:24 -0500

    carcontroller

--- src/main/java/io/ascending/training/controller/CarController.java
+++ src/main/java/io/ascending/training/controller/CarController.java
@@ -5,10 +5,13 @@ import io.ascending.training.repository.CarRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/cars", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -26,8 +29,18 @@ public class CarController {
         return result;
     }
 
+
+    @Cacheable(value = "cars")
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Car> getAllCars() {
+        logger.info("Getting all cars");
+        List<Car> result = carRepository.findAll();
+        return result;
+    }
+
     @CachePut(value = "cars", key = "#car.id", unless = "#car.brand == null")
     @RequestMapping(method = RequestMethod.POST)
+//    @CacheEvict(value = "cars", allEntries = true)
     public Car generateCar(@RequestBody Car car) {
         return carRepository.save(car);
     }
